945b2191b12cb46a97eccc6ffbfaf907c14ba76f
m-oka-system <m-oka-system@outlook.com>
2025-09-27 18:19:36 +0900

    feat: Key Vaultにシークレットを登録するワークフローを追加

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "KEY_VAULT_NAME" {
+  value = azurerm_key_vault.this.name
+}
+
 output "STORAGE_ACCOUNT_PRIMARY_KEY" {
   value     = azurerm_storage_account.this.primary_access_key
   sensitive = true
